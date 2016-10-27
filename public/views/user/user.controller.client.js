bfa5147132dfcf78c4cd089cf5bc53c599e5e751
Vineeth Ravindra <vineeth89@gmail.com>
2016-10-26 22:17:38 -0400

    Some changes looking good but need major changes

--- public/views/user/user.controller.client.js
+++ public/views/user/user.controller.client.js
@@ -1,8 +1,9 @@
 (function(){
     angular
         .module("Connecxion")
-        .controller("LandingPageController", LandingPageController);
-    function LandingPageController($http, $sce,$mdSidenav) {
+        .controller("LandingPageController", LandingPageController)
+        .controller("DialogController",DialogController);
+    function LandingPageController($http, $sce,$mdSidenav,$mdDialog) {
         var vm = this;
         vm.data = [];
         $http.get('./assests/connexion-dummy.json').success(function (data) {
@@ -44,5 +45,20 @@
 
             return $sce.trustAsResourceUrl(url);
         }
+        vm.showAdd = function(ev) {
+            $mdDialog.show({
+                controller: DialogController,
+                template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
+                targetEvent: ev,
+            })
+                .then(function(answer) {
+                    $scope.alert = 'You said the information was "' + answer + '".';
+                }, function() {
+                    $scope.alert = 'You cancelled the dialog.';
+                });
+        };
+    }
+    function DialogController($http, $sce,$mdSidenav,$mdDialog){
+        console.log("Hello");
     }
 })();    
