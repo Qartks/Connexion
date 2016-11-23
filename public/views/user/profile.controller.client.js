2c4135bced69059b2e044cee128167591eafa06a
Kartikeya Shukla <shukla.ka@husky.neu.edu>
2016-11-23 11:58:50 -0500

    progress?

--- public/views/user/profile.controller.client.js
+++ public/views/user/profile.controller.client.js
@@ -3,7 +3,14 @@
         .module("Connexion")
         .controller("ProfileController", ProfileController);
 
-    function ProfileController() {
+    function ProfileController($http) {
+
+        var vm = this;
+        vm.data = [];
+        $http.get('./assests/connexion-dummy.json').success(function (data) {
+            vm.data = data;
+            vm.data = vm.data.slice(0, 4);
+        });
 
     }
 
