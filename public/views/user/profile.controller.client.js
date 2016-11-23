a674c20d3f5b1867837f175fedb6f9cb93e71611
Kartikeya Shukla <shukla.ka@husky.neu.edu>
2016-11-23 16:45:10 -0500

    A bit more progress

--- public/views/user/profile.controller.client.js
+++ public/views/user/profile.controller.client.js
@@ -6,11 +6,19 @@
     function ProfileController($http) {
 
         var vm = this;
+
+        this.isthispostmine = isthispostmine;
+
         vm.data = [];
         $http.get('./assests/connexion-dummy.json').success(function (data) {
             vm.data = data;
-            vm.data = vm.data.slice(0, 4);
+            vm.data = vm.data.slice(5, 15);
         });
+
+
+        function isthispostmine(post) {
+            return post.isOpen ? "green" : "blue";
+        }
 
     }
 
