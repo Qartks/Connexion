6a7cbe3aaf55c17236f6a075443ec296a435d1fd
Vineeth Ravindra <vineeth89@gmail.com>
2016-11-24 13:16:16 -0500

    adding login view and controller

---
+++ public/views/user/login.controller.client.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Vineeth on 11/24/16.
+ */
+(function () {
+    angular
+        .module("Connexion")
+        .controller("UserLoginController", UserLoginController);
+    function UserLoginController(){
+        var vm = this;
+    }
+})();
