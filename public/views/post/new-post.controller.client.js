7d5b7dafc08bb217dfc827df1254020465a5ea5b
Vineeth Ravindra <vineeth89@gmail.com>
2016-12-03 18:17:27 -0500

    Flicker Support for new post. And linking page on landing page

--- public/views/post/new-post.controller.client.js
+++ public/views/post/new-post.controller.client.js
@@ -1,8 +1,32 @@
 (function () {
     angular
         .module("Connexion")
-        .controller("NewPostController", NewPostController);
-    function NewPostController() {
+        .controller("NewPostController", NewPostController)
+    function NewPostController($routeParams,$location,$sce,FlickerService) {
         var vm = this;
+        vm.searchPhotos = searchPhotos;
+        vm.selectPhoto = selectPhoto;
+        function init() {
+            vm.photos = vm.searchPhotos("Nature");
+        }
+        init()
+        function searchPhotos(searchTerm) {
+            vm.photos = null;
+            FlickerService
+                .searchPhotos(searchTerm)
+                .then(function(response) {
+                    data = response.data.replace("jsonFlickrApi(","");
+                    data = data.substring(0,data.length - 1);
+                    data = JSON.parse(data);
+                    vm.photos = data.photos;
+                    console.log(vm.photos)
+                });
+        }
+        function selectPhoto(photo) {
+            var url = "https://farm" + photo.farm + ".staticflickr.com/"
+                + photo.server + "/" + photo.id + "_" + photo.secret + "_b.jpg";
+            console.log(url);
+        }
     }
+
 })();
